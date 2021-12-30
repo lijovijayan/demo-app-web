3baa288d145b6cb96f072ca93d781c60f587955c
lijovijayan <lijovijayan00@gmail.com>
2021-12-30 22:57:23 +0530

    updated dev base url

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,7 +1,7 @@
 let BASE_URL = process.env.BASE_URL
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    BASE_URL = 'https://api.lijovijayan.dev'
+    BASE_URL = 'http://localhost:3500'
 } else {
     BASE_URL = 'https://api.lijovijayan.dev'
 }
